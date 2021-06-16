5d90dfe6d631186b516d1a34770ea70120a28fb9
Nicolás Dalmás <ndalmas9@gmail.com>
2021-06-15 22:44:54 -0300

    Revert "test"
    This reverts commit c702237a050cc3108a86fea518eba9f391bb5602.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import logo from './logo.svg';
 import './App.css';
 
 import List from "./components/List";
+import Create from "./components/Create";
+
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
     <div className="container">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <div class="nav navbar-nav">
+          <a class="nav-item nav-link active" href="/">System <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="/create">Create Employe</a>
+      </div>
+    </nav>
+
+    <img src={logo} className="App-logo" alt="logo" />
 
     <Route exact path="/" component={List}></Route>
+    <Route path="/create" component={Create}></Route>
+
     </div>
     </Router>
   );
