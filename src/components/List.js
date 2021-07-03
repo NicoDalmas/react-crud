2eb9a579f36dcc5210b5d325c7d121f374966d28
Nicolás Dalmás <ndalmas9@gmail.com>
2021-07-03 22:28:36 +0200

    Consuming API from URL & show it
    Consuming sample data from:
    https://jsonplaceholder.typicode.com/users
    
    AND display data in a table.

--- src/components/List.js
+++ src/components/List.js
@@ -5,13 +5,21 @@ import { Link } from "react-router-dom";
 class List extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            loadedData:false,
+            employees: []
+        }
     }
 
     loadData(){
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(respuesta=>respuesta.json())
-        .then((datosRespuesta)=>{console.log(datosRespuesta)})
+        .then((datosRespuesta)=>{
+            
+            console.log(datosRespuesta);
+            this.setState({ loadedData:true, employees:datosRespuesta })
+        })
+
         .catch(console.log)
     }
 
@@ -20,29 +28,41 @@ class List extends React.Component {
     }
 
     render() { 
-        return ( <table className="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td >1</td>
-                    <td>Nicolas</td>
-                    <td>ndalmas9@gmail.com</td>
-                    <td>
-                       <div className="btn-group" role="group" aria-label="">
-                           <Link className="btn btn-warning" to={"/edit"}>Edit</Link>
-                           <button type="button" className="btn btn-danger">Delete</button>
-                       </div> 
-                    </td>
-                </tr>
-            </tbody>
-        </table>);
+        const{loadedData, employees}=this.state
+
+        if(!loadedData){ return(<div>Cargando...</div>); }
+        else{
+            return ( <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map(
+                        (employee)=>(
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.name}</td>
+                                <td>{employee.email}</td>
+                                <td>
+                                <div className="btn-group" role="group" aria-label="">
+                                    <Link className="btn btn-warning" to={"/edit"}>Edit</Link>
+                                    <button type="button" className="btn btn-danger">Delete</button>
+                                </div> 
+                                </td>
+                            </tr>   
+                        )
+                    )}
+
+
+
+                </tbody>
+            </table>);
+        }
     }
 }
  
